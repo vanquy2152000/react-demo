b971deaaaec0e066213cb5a4bad62017785d709b
Cao Van Quy <quy.187pm20553@vanlanguni.vn>
2022-09-18 11:56:53 +0700

    design new header

--- src/component/Home/HomePage.js
+++ src/component/Home/HomePage.js
@@ -10,6 +10,17 @@ const HomePage = (props) => {
                 >
                 </source>
             </video>
+            <div className="homepage-content">
+                <div className="title-1">There's a better way to ask</div>
+                <div className="title-2">You don't want to make a boring form.
+                    And your audience won't answer one. Create a typeform instead-and make everyone happy.
+                </div>
+                <div className="title-3">
+                    <button className="btn-getstart">
+                        Get started - it's free
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
